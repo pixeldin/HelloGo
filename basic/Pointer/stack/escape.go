a6738f112e8fecc160d6823e35c94fa12f7a042c
pixeldin <626995617@qq.com>
2020-01-15 09:47:41 +0800

    escape 2 heap demo

--- basic/Pointer/stack/escape.go
+++ basic/Pointer/stack/escape.go
@@ -2,6 +2,16 @@ package stack
 
 //Try to escape
 
+func SpecifySizeAllocate()  {
+	buf := make([]byte, 5)
+	println(buf)
+}
+
+func UnSpecifySizeAllocate(size int)  {
+	buf := make([]byte, size)
+	println(buf)
+}
+
 //go:noinline
 func CreatePointer() *int  {
 	//a := int(1)
