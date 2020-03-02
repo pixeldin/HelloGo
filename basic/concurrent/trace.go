16fdc9fde167c52593c4739c224c1c473768542d
pixeldin <626995617@qq.com>
2020-03-02 17:14:48 +0800

    trace demo

--- basic/concurrent/trace.go
+++ basic/concurrent/trace.go
@@ -1,35 +1,32 @@
 package main
 
 import (
-	"net/http"
-	"runtime"
+	"fmt"
+	"os"
+	"runtime/trace"
 	"sync"
 	"time"
 )
 
 func main() {
 
-	//var total = new(int)
-
-	runtime.GOMAXPROCS(1)
-	go func() {
-		for {
-			//保持空转,如果没有其他函数调用,在单个处理器下该协程不会切换
-			;
-
-			//函数调用, 触发协程切换(主协程)
-			//Callsomething(total)
-			//runtime.Gosched()
-		}
-	}()
+	//runtime.GOMAXPROCS(1)
+	//go func() {
+	//	for {
+	//		//保持空转,如果没有其他函数调用,在单个处理器下该协程不会切换
+	//		;
+	//
+	//		//函数调用, 触发协程切换(主协程)
+	//		//Callsomething(total)
+	//		//runtime.Gosched()
+	//	}
+	//}()
 
 	//尝试切换协程
-	time.Sleep(100)
-	println("main done.")
-}
+	//time.Sleep(100)
+	//println("main done.")
 
-func Callsomething(total *int) {
-	println(*total)
+	TraceDemo()
 }
 
 func TraceDemo() {
@@ -38,21 +35,33 @@ func TraceDemo() {
 		go run trace.go 2> trace.out
 		go tool trace trace.out
 	*/
-	//trace.Start(os.Stderr)
-	//defer trace.Stop()
+	trace.Start(os.Stderr)
+	defer trace.Stop()
 
 	var wg sync.WaitGroup
+	var unsafeA = 0
+	var safeA = 0
+	var mut sync.Mutex
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 20; i++ {
 		wg.Add(1)
 
 		go func() {
-			http.Get(`https://httpstat.us/200?sleep=10000`)
-			//println("Do something")
-			//time.Sleep(100)
+
+			time.Sleep(7)
+			unsafeA++
+
+			mut.Lock()
+			safeA++
+			mut.Unlock()
+			//http.Get(`https://httpstat.us/200?sleep=10000`)
+			//fmt.Printf("Do something of i: %d\n", i)
 			wg.Done()
 		}()
 	}
 
 	wg.Wait()
+
+	fmt.Println(unsafeA)
+	fmt.Println(safeA)
 }
