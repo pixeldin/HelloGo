869b4d83860a4cb9bafd1bf7461e11035f1cf5f3
pixeldin <626995617@qq.com>
2021-05-27 10:14:55 +0800

    unwarp safe.Go()

--- basic/consumeQ/queue.go
+++ basic/consumeQ/queue.go
@@ -1,7 +1,6 @@
 package consumeQ
 
 import (
-	"HelloGo/common/safego"
 	"fmt"
 	"log"
 	"time"
@@ -18,7 +17,8 @@ func MqBackupToMongo() {
 	msgIdChannel := make(chan string, QUEUE_SIZE)
 
 	// 生产端
-	safego.Go(func() {
+	//safego.Go(func() {
+	go func() {
 		//应对redis重启的情况
 		for {
 			//ctx := context.Background()
@@ -38,7 +38,7 @@ func MqBackupToMongo() {
 					time.Sleep(100)
 					count++
 					if count == 15 {
-						//panic("haha")
+						panic("haha")
 						//break
 						time.Sleep(10 * time.Second)
 						count = 0
@@ -47,12 +47,14 @@ func MqBackupToMongo() {
 			}()
 
 			fmt.Println("just ticking...")
+			// 保护性休眠
 			time.Sleep(time.Second)
 		}
-	})
+	}()
 
 	// 消费端
-	safego.Go(func() {
+	//safego.Go(func() {
+	go func() {
 		msgIds := make([]string, 0)
 		timer := time.NewTimer(time.Second * 3)
 		defer timer.Stop()
@@ -69,7 +71,7 @@ func MqBackupToMongo() {
 				// 重置计时器
 				timer.Reset(time.Second * 3)
 			case <-timer.C:
-				// 30秒执行一次收尾工作
+				// 首次或者在每次Reset之后的3秒执行一次收尾工作
 				if len(msgIds) > 0 {
 					// process msgIds with less
 					fmt.Println("Timer: Process batch msg with size: ", len(msgIds))
@@ -77,5 +79,5 @@ func MqBackupToMongo() {
 				}
 			}
 		}
-	})
+	}()
 }
